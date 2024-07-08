1c3c479a42a688dd0842c5aabe4cb474e9790e86
JamalSQ <qadrij688@gmail.com>
2024-07-08 09:52:01 +0500

    addTeacherDesignAdded

--- app/src/main/java/com/android/cuifypmanagementsystem/adapters/BatchAdapter.kt
+++ app/src/main/java/com/android/cuifypmanagementsystem/adapters/BatchAdapter.kt
@@ -35,7 +35,6 @@ class BatchAdapter(var batches : List<Batch>, val context : Context)  : Recycler
     override fun onBindViewHolder(holder: BatchViewHolder, position: Int) {
         holder.name.text = batches[position].name
         holder.semester.text = batches[position].semester.toString()
-
     }
 
     override fun getItemCount() = batches.size
