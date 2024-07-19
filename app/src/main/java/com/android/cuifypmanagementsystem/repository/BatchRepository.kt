837c10e2fdc895054dd8e20f05c84dedfd898e43
MudassarStd <119745592+MudassarStd@users.noreply.github.com>
2024-07-19 20:15:02 +0500

    Update BatchRepository.kt

--- app/src/main/java/com/android/cuifypmanagementsystem/repository/BatchRepository.kt
+++ app/src/main/java/com/android/cuifypmanagementsystem/repository/BatchRepository.kt
@@ -6,18 +6,13 @@ import com.android.cuifypmanagementsystem.room.MainDatabase
 
 class BatchRepository(app : Context) {
     private val db = MainDatabase.getDatabase(app).batchDao()
-    suspend fun insert(batch : Batch)
-    {
-        db.insert(batch)
-    }
-    suspend fun update(batch : Batch)
-    {
-        db.update(batch)
-    }
-    suspend fun delete(batch : Batch)
-    {
-        db.delete(batch)
-    }
+    
+    suspend fun insert(batch : Batch) = db.insert(batch)
+    
+    suspend fun update(batch : Batch) = db.update(batch)
+
+    suspend fun delete(batch : Batch) = db.delete(batch)
+    
     suspend fun getAllBatches(): List<Batch> {
         return db.getAllBatches()
     }
